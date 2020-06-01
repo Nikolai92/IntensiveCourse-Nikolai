34c25dc953c11b5071ae8d5ad543fe26cfb4158f
nikolai92 <nikolai.ibanez@gmail.com>
2020-06-01 18:36:56 -0400

    Baked Navmesh/AI/AI Speed/Walk anim working

---
+++ Assets/GameDevHQ/FileBase/Projects/Tutorials/Starter_Files/Epic_Tower_Defense/Scripts/AI.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AI : MonoBehaviour
+{
+
+    [SerializeField] private GameObject _target;
+
+    private NavMeshAgent _agent; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _agent = GetComponent<NavMeshAgent>();
+        _agent.SetDestination(_target.transform.position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
