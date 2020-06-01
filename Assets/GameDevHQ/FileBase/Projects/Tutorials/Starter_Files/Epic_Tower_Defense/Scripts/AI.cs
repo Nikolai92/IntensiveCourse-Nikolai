32c73ae87fbf5e5bfb0d81aad1bd614adc3358f0
nikolai92 <nikolai.ibanez@gmail.com>
2020-06-01 19:22:38 -0400

    AI System working properly

--- Assets/GameDevHQ/FileBase/Projects/Tutorials/Starter_Files/Epic_Tower_Defense/Scripts/AI.cs
+++ Assets/GameDevHQ/FileBase/Projects/Tutorials/Starter_Files/Epic_Tower_Defense/Scripts/AI.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class AI : MonoBehaviour
+public class AI : Enemy
 {
 
     [SerializeField] private GameObject _target;
---
+++ Assets/GameDevHQ/FileBase/Projects/Tutorials/Starter_Files/Epic_Tower_Defense/Scripts/Enemy.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Enemy : MonoBehaviour
+{
+   [SerializeField] private int _health = 0;
+
+   [SerializeField] private int _warFund = 0;
+}
