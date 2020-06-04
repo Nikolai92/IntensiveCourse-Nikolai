7cb609d7667ec5e20f1a957823113d07925b5fca
nikolai92 <nikolai.ibanez@gmail.com>
2020-06-04 13:55:06 -0400

    Working on Recycler

---
+++ Assets/GameDevHQ/FileBase/Projects/Tutorials/Starter_Files/Epic_Tower_Defense/Scripts/EndRecycler.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndRecycler : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision other)
+    {
+        other.gameObject.SetActive(false);
+        Debug.Log("IM COLLIDING");
+    }
+}
