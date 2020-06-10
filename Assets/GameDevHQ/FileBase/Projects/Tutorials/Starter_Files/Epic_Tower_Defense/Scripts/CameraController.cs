9e51f4979f154de6e60c232e0116ae1ada441781
nikolai92 <nikolai.ibanez@gmail.com>
2020-06-10 01:03:17 -0400

    Added Cam controls, Need Clamp and Fixing Orientation

--- Assets/GameDevHQ/FileBase/Projects/Tutorials/Starter_Files/Epic_Tower_Defense/Scripts/CameraController.cs
+++ Assets/GameDevHQ/FileBase/Projects/Tutorials/Starter_Files/Epic_Tower_Defense/Scripts/CameraController.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     [SerializeField] private int _camSpeed = 100;
-    [SerializeField] private int _scrollSpeed = 40;
+    [SerializeField] private int _scrollSpeed = 100;
+
+    [SerializeField] private float _mouseScreenPercentile = .2f;
+    [SerializeField] private float _mouseScrollSensitivity = 1f;
+
     [SerializeField] private Camera GameCam;
-
-
 
     void Start()
     {
@@ -17,6 +19,7 @@ public class CameraController : MonoBehaviour
 
     void Update()
     {
+        MouseCamCtrl();
         CamCtrl();
         GameCam.fieldOfView = Mathf.Clamp(GameCam.fieldOfView, 10, 50);
     }
@@ -32,4 +35,53 @@ public class CameraController : MonoBehaviour
         //zoom in/out
         GameCam.fieldOfView += Input.mouseScrollDelta.y * _scrollSpeed * Time.deltaTime;
     }
+
+    private void MouseCamCtrl()
+    {
+        if (Input.mousePosition.x < PercentileWidthMin())
+        {
+            transform.Translate(Vector3.left * Time.deltaTime);
+        }
+        else if (Input.mousePosition.x > PercentileWidthMax())
+        {
+            transform.Translate(Vector3.right * Time.deltaTime);
+        }
+        else if (Input.mousePosition.y < PercentileHeightMin())
+        {
+            transform.Translate(Vector3.down * Time.deltaTime);
+        }
+        else if (Input.mousePosition.y > PercentileHeightMax())
+        {
+            transform.Translate(Vector3.up * Time.deltaTime);
+        }
+    }
+
+    private float PercentileWidthMin()
+    {
+        float mouseBorderWidth = _mouseScreenPercentile * Screen.width;
+        return mouseBorderWidth;
+    }
+
+    private float PercentileWidthMax()
+    {
+        float mouseBorderWidth = Screen.width - (_mouseScreenPercentile * Screen.width);
+        return mouseBorderWidth;
+    }
+
+    private float PercentileHeightMin()
+    {
+        float MouseBorderHeight = _mouseScreenPercentile * Screen.height;
+        return MouseBorderHeight;
+    }
+    private float PercentileHeightMax()
+    {
+        float MouseBorderHeight = Screen.height - (_mouseScreenPercentile * Screen.height);
+        return MouseBorderHeight;
+    }
+
+
+
+
+
+
 }
