f39fb4a684a58240ae6c03c639d826b9f664f6b6
nikolai92 <nikolai.ibanez@gmail.com>
2020-06-11 04:00:03 -0400

    Implementing Tower Positioning/Particle event

---
+++ Assets/GameDevHQ/FileBase/Projects/Tutorials/Starter_Files/Epic_Tower_Defense/Scripts/LocationScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LocationScript : MonoBehaviour
+{
+    public void Start()
+    {
+        DeactivateParticles();
+    }
+
+    private void OnEnable()
+    {
+        TowerPlacement.placeTower += ActivateParticles;
+        TowerPlacement.towerPlaced += DeactivateParticles;
+    }
+
+    private void OnDisable()
+    {
+        TowerPlacement.placeTower -= ActivateParticles;
+        TowerPlacement.towerPlaced -= DeactivateParticles;
+    }
+
+    public void ActivateParticles()
+    {
+        GetComponentInChildren<ParticleSystem>().Play();
+    }
+
+    public void DeactivateParticles()
+    {
+        GetComponentInChildren<ParticleSystem>().Stop();
+    }
+}
---
+++ Assets/GameDevHQ/FileBase/Projects/Tutorials/Starter_Files/Epic_Tower_Defense/Scripts/TowerPlacement.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerPlacement : MonoBehaviour
+{
+    [SerializeField] private GameObject[] _decoyTowers;
+    [SerializeField] private GameObject[] _towers;
+
+    public static Action placeTower;
+    public static Action towerPlaced;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MouseCtrl();
+
+        
+    }
+
+    private void MouseCtrl()
+    {
+        Ray rayOrigin = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hitInfo;
+
+        if (Physics.Raycast(rayOrigin, out hitInfo))
+        {
+            _decoyTowers[0].transform.position = hitInfo.point;
+
+            if (Input.GetKeyDown(KeyCode.Alpha1))
+            {
+                if (placeTower != null)
+                {
+                    placeTower();
+                }
+            }
+
+            if (Input.GetMouseButtonDown(0) && (hitInfo.collider.tag == "ValidSpot"))
+            {
+                Instantiate(_decoyTowers[0], hitInfo.collider.transform.position, Quaternion.identity);
+                towerPlaced();
+            }
+
+        }
+    }
+}
